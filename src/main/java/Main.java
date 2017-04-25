202747eff235d3125ff9f63a9950c1dace449c38
Dharmesh Kakadia <dharmesh.kakadia@gmail.com>
2017-04-25 15:10:53 -0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.apache.log4j.Logger;
+
+/**
+ * Created by dkakadia on 4/24/2017.
+ */
+public class Main {
+	static final Logger logger = Logger.getLogger(Main.class);
+	public static void main(String[] args) {
+		logger.info("Hello World");
+	}
+}
